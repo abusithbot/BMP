d5b04dacbb5bbd5654b45a2d20173cc1ae766fa7
abusithbot <aboubakar.03@hotmail.com>
2023-11-20 12:15:56 +0100

    reorganisation
    sprite editor perso

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float jump;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+          {
+            rb.AddForce(Vector2.up * jump);
+          }
     }
 }
